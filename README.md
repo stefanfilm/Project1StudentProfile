8306850fd5208b56338351537c1d83f1a0e54116
marissamelo91 <marissa.melo91@yahoo.com>
2024-05-15 22:15:11 -0700

    updated html, css, and javascript

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Project1StudentProfile
 
-Adding change to README 5
+# Student Profile
+
+## Description
+Our motivation to create this project was to make a page where students could enter in unique information to help others get to know them better.  We built this project to make a site where students can share information and build their profiles. This site solves the problem of not having a place for students to use APIs to share information about themselves. We learned how to use web APIs, Javascript, and Bulma to achieve the results.
+
+## Deployed Site
+The deployed website can be reached at the following link.
+https://stefanfilm.github.io/Project1StudentProfile/
+
+## Installation
+To install the project, pull the code from https://github.com/stefanfilm/Project1StudentProfile and open it in a code editor. Open the script.js or postsscript.js file in the code editor and view the JavaScript. You could open the index.html file in a browser to see the code function.
+
+## Usage
+To use the website, enter text into the various input fields and click submit. Click "choose file" to add a profile picture to your profile. When you click the submit button on the location label, you will see a map showing you where you are based on your IP address. When you enter a movie title to the movie input and click submit, you will see a poster of the movie you chose. If you enter a link to your social media website, once you click submit, you could visit the website by clicking on the icon.
+    ```md
+    ![Landing Page](./images/Homepage1.png)
+    ![Landing Page](./images/Homepage2.png)
+    ```
+## Credits
+We worked on this project under the instruction of Mark Carlson in the UC Berkeley Software Developer Bootcamp.
+
+## License
+The license is an MIT license. Please refer to the GitHub Repository for details on the license.
+
+## Badges
+![HTML-CSS](https://img.shields.io/badge/HTML-CSS-green)
+![JavaScript](https://img.shields.io/badge/JavaScript-blue)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -107,8 +107,12 @@ movieBtn.addEventListener("click", async function () {
             name: movieName,
             imageUrl: movieUrl
         };
-        const movieDataString = JSON.stringify(movieData);
-        localStorage.setItem('savedMovie', movieDataString);
+
+        const movieCollection = JSON.parse(localStorage.getItem("movieCollection")) || [];
+        movieCollection.push(movieData)
+
+        // const movieDataString = JSON.stringify(movieData);
+        localStorage.setItem("movieCollection",JSON.stringify(movieCollection));
 
     } catch (error) {
         console.error('Error fetching movie data:', error);
@@ -120,13 +124,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const occupationBtn = document.getElementById("occupation-btn");
     const occupationInput = document.getElementById("occupation-input");
     const occupationLabel = document.getElementById("occupation-label")
+    const savedOccupation = localStorage.getItem("saved-Occupation")
+    if (savedOccupation) {
+        occupationLabel.textContent = savedOccupation;
+        occupationInput.value = savedOccupation;
+        occupationInput.style.display = "none";
+        occupationBtn.textContent = "Edit";
+    }
     occupationBtn.addEventListener("click", function() {
         if (occupationBtn.textContent === "Submit") {
             if (occupationInput.value.trim() !== "") {
                 occupationLabel.textContent = occupationInput.value;
+                localStorage.setItem("saved-Occupation", occupationInput.value);
                 occupationInput.style.display = "none";
                 occupationBtn.textContent = "Edit";
-            } 
+            }
         } else if (occupationBtn.textContent === "Edit") {
             occupationLabel.textContent = "";
             occupationInput.style.display = "";
@@ -140,13 +152,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const ageBtn = document.getElementById("age-btn");
     const ageInput = document.getElementById("age-input");
     const ageLabel = document.getElementById("age-label")
+    const savedAge = localStorage.getItem("saved-age");
+    if (savedAge) {
+        ageLabel.textContent = savedAge;
+        ageInput.value = savedAge;
+        ageInput.style.display = "none";
+        ageBtn.textContent = "Edit";
+    }
     ageBtn.addEventListener("click", function() {
         if (ageBtn.textContent === "Submit") {
             if (ageInput.value.trim() !== "") {
                 ageLabel.textContent = ageInput.value;
+                localStorage.setItem("saved-age", ageInput.value);
                 ageInput.style.display = "none";
                 ageBtn.textContent = "Edit";
-            } 
+            }
         } else if (ageBtn.textContent === "Edit") {
             ageLabel.textContent = "";
             ageInput.style.display = "";
@@ -159,13 +179,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const aboutMeBtn = document.getElementById("aboutme-btn");
     const aboutMeInput = document.getElementById("aboutme-input");
     const aboutMeLabel = document.getElementById("aboutme-label")
+    const savedAboutMe = localStorage.getItem("saved-about-me")
+    if (savedAboutMe) {
+        aboutMeLabel.textContent = savedAboutMe;
+        aboutMeInput.value = savedAboutMe;
+        aboutMeInput.style.display = "none";
+        aboutMeBtn.textContent = "Edit";
+    }
     aboutMeBtn.addEventListener("click", function() {
         if (aboutMeBtn.textContent === "Submit") {
             if (aboutMeInput.value.trim() !== "") {
                 aboutMeLabel.textContent = aboutMeInput.value;
+                localStorage.setItem("saved-about-me", aboutMeInput.value);
                 aboutMeInput.style.display = "none";
                 aboutMeBtn.textContent = "Edit";
-            } 
+            }
         } else if (aboutMeBtn.textContent === "Edit") {
             aboutMeLabel.textContent = "";
             aboutMeInput.style.display = "";
@@ -179,13 +207,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const bookBtn = document.getElementById("book-btn");
     const bookInput = document.getElementById("book-input");
     const bookLabel = document.getElementById("book-label")
+    const savedBook = localStorage.getItem("saved-book");
+    if (savedBook) {
+        bookLabel.textContent = savedBook;
+        bookInput.value = savedBook;
+        bookInput.style.display = "none";
+        bookBtn.textContent = "Edit";
+    }
     bookBtn.addEventListener("click", function() {
         if (bookBtn.textContent === "Submit") {
             if (bookInput.value.trim() !== "") {
                 bookLabel.textContent = bookInput.value;
+                localStorage.setItem("saved-book", bookInput.value);
                 bookInput.style.display = "none";
                 bookBtn.textContent = "Edit";
-            } 
+            }
         } else if (bookBtn.textContent === "Edit") {
             bookLabel.textContent = "";
             bookInput.style.display = "";
@@ -199,13 +235,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const artistBtn = document.getElementById("artist-btn");
     const artistInput = document.getElementById("artist-input");
     const artistLabel = document.getElementById("artist-label")
+    const savedArtist = localStorage.getItem("saved-artist");
+    if (savedArtist) {
+        artistLabel.textContent = savedArtist;
+        artistInput.value = savedArtist;
+        artistInput.style.display = "none";
+        artistBtn.textContent = "Edit";
+    }
     artistBtn.addEventListener("click", function() {
         if (artistBtn.textContent === "Submit") {
             if (artistInput.value.trim() !== "") {
                 artistLabel.textContent = artistInput.value;
+                localStorage.setItem("saved-artist", artistInput.value);
                 artistInput.style.display = "none";
                 artistBtn.textContent = "Edit";
-            } 
+            }
         } else if (artistBtn.textContent === "Edit") {
             artistLabel.textContent = "";
             artistInput.style.display = "";
@@ -214,18 +258,26 @@ document.addEventListener("DOMContentLoaded", function() {
     })
 })
 
-//Artist Function 
+//Meal Function 
 document.addEventListener("DOMContentLoaded", function() {
     const mealBtn = document.getElementById("meal-btn");
     const mealInput = document.getElementById("meal-input");
     const mealLabel = document.getElementById("meal-label")
+    const savedMeal = localStorage.getItem("saved-meal");
+    if (savedMeal) {
+        mealLabel.textContent = savedMeal;
+        mealInput.value = savedMeal;
+        mealInput.style.display = "none";
+        mealBtn.textContent = "Edit";
+    }
     mealBtn.addEventListener("click", function() {
         if (mealBtn.textContent === "Submit") {
             if (mealInput.value.trim() !== "") {
                 mealLabel.textContent = mealInput.value;
+                localStorage.setItem("saved-meal", mealInput.value);
                 mealInput.style.display = "none";
                 mealBtn.textContent = "Edit";
-            } 
+            }
         } else if (mealBtn.textContent === "Edit") {
             mealLabel.textContent = "";
             mealInput.style.display = "";
@@ -234,4 +286,211 @@ document.addEventListener("DOMContentLoaded", function() {
     })
 })
 
-   
+//Profile Picture 
+const _ReadImage = (event) => {
+    const input = event.target;
+    const status = document.querySelector("#status");
+    status.innerText = `Reading File...`;
+    if(input.files && input.files[0]) {
+      const reader = new FileReader();
+      reader.onload = e => {
+        const imgData = e.target.result;
+        _SaveImage(imgData);
+        _LoadImage(imgData)
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  const _SaveImage = (imgData) => {
+    localStorage.setItem("profile-pic", imgData);
+  };
+  const _LoadImage = (imgData) => {
+    const profilePic = document.querySelector("#profile-pic");
+    const status = document.querySelector("#status");
+    if(imgData) {
+      profilePic.src = imgData;
+      status.innerText = `Image Loaded!`;
+    } else {
+      profilePic.src = "";
+      status.innerText = `No Image!`;
+    }
+  }
+  _LoadImage(localStorage.getItem("profile-pic")); 
+
+  //Birthday Picker 
+  document.addEventListener("DOMContentLoaded", function() {
+    const birthdayBtn = document.getElementById("birthday-btn");
+    const birthdayInput = document.getElementById("datepicker");
+    const birthdayLabel = document.getElementById("birthday-label")
+    const savedBirthday = localStorage.getItem("saved-birthday");
+    if (savedBirthday) {
+        birthdayLabel.textContent = savedBirthday;
+        birthdayInput.value = savedBirthday;
+        birthdayInput.style.display = "none";
+        birthdayBtn.textContent = "Edit";
+    }
+    birthdayBtn.addEventListener("click", function() {
+        if (birthdayBtn.textContent === "Submit") {
+            if (birthdayInput.value.trim() !== "") {
+                birthdayLabel.textContent = birthdayInput.value;
+                localStorage.setItem("saved-birthday", birthdayInput.value);
+                birthdayInput.style.display = "none";
+                birthdayBtn.textContent = "Edit";
+            }
+        } else if (birthdayBtn.textContent === "Edit") {
+            birthdayLabel.textContent = "";
+            birthdayInput.style.display = "";
+            birthdayBtn.textContent = "Submit";
+        }
+    })
+})
+
+
+//Name 
+document.addEventListener("DOMContentLoaded", function() {
+    const nameBtn = document.getElementById("name-btn");
+    const nameInput = document.getElementById("name-input");
+    const nameLabel = document.getElementById("name-label")
+    const savedName = localStorage.getItem("savedName");
+    if (savedName) {
+        nameLabel.textContent = savedName;
+        nameInput.value = savedName;
+        nameInput.style.display = "none";
+        nameBtn.textContent = "Edit";
+    }
+    nameBtn.addEventListener("click", function() {
+        if (nameBtn.textContent === "Submit") {
+            if (nameInput.value.trim() !== "") {
+                nameLabel.textContent = nameInput.value;
+                localStorage.setItem("savedName", nameInput.value);
+                nameInput.style.display = "none";
+                nameBtn.textContent = "Edit";
+            }
+        } else if (nameBtn.textContent === "Edit") {
+            nameLabel.textContent = "";
+            nameInput.style.display = "";
+            nameBtn.textContent = "Submit";
+        }
+    })
+})
+
+//Facebook
+document.addEventListener("DOMContentLoaded", function() {
+    const fbBtn = document.getElementById("facebook-button");
+    const fbInput = document.getElementById("facebook-input");
+    const fbIcon = document.getElementById("facebook-icon");
+    const fbAtt = document.getElementById("facebook-link-att");
+    const savedFb = localStorage.getItem("saved-fb");
+    if (savedFb) {
+        fbAtt.href = savedFb;
+        fbIcon.style.pointerEvents = "auto";
+        fbInput.style.display = "none";
+        fbBtn.textContent = "Edit";
+    }
+    fbBtn.addEventListener("click", function() {
+        if (fbBtn.textContent === "Submit") {
+            if (fbInput.value.trim() !== "") {
+                let enteredURL = fbInput.value.trim();
+                if (!enteredURL.startsWith("http://") && !enteredURL.startsWith("https://")) {
+                    enteredURL = "https://" + enteredURL;
+                }
+                fbAtt.href = enteredURL;
+                fbIcon.style.pointerEvents = "auto";
+                localStorage.setItem("saved-fb", enteredURL);
+                fbInput.style.display = "none";
+                fbBtn.textContent = "Edit";
+            }
+        } else if (fbBtn.textContent === "Edit") {
+            fbAtt.removeAttribute("href");
+            fbIcon.style.pointerEvents = "none";
+            fbInput.style.display = "";
+            fbBtn.textContent = "Submit";
+        }
+    })
+})
+
+//Linkedin
+document.addEventListener("DOMContentLoaded", function() {
+    const linkedInBtn = document.getElementById("linkedin-button");
+    const linkedInInput = document.getElementById("linkedin-input");
+    const linkedInIcon = document.getElementById("linkedin-icon");
+    const linkedInAtt = document.getElementById("linkedin-link-att");
+    const savedLinkedIn = localStorage.getItem("saved-linkedin");
+    if (savedLinkedIn) {
+        linkedInAtt.href = savedLinkedIn;
+        linkedInIcon.style.pointerEvents = "auto";
+        linkedInInput.style.display = "none";
+        linkedInBtn.textContent = "Edit";
+    }
+    linkedInBtn.addEventListener("click", function() {
+        if (linkedInBtn.textContent === "Submit") {
+            if (linkedInInput.value.trim() !== "") {
+                let enteredURL = linkedInInput.value.trim();
+                if (!enteredURL.startsWith("http://") && !enteredURL.startsWith("https://")) {
+                    enteredURL = "https://" + enteredURL;
+                }
+                linkedInAtt.href = enteredURL;
+                linkedInIcon.style.pointerEvents = "auto";
+                localStorage.setItem("saved-linkedin", enteredURL);
+                linkedInInput.style.display = "none";
+                linkedInBtn.textContent = "Edit";
+            }
+        } else if (linkedInBtn.textContent === "Edit") {
+            linkedInAtt.removeAttributeNS("href");
+            linkedInIcon.style.pointerEvents = "none";
+            linkedInInput.style.display = "";
+            linkedInBtn.textContent = "Submit";
+        }
+    })
+})
+
+//Instagram
+document.addEventListener("DOMContentLoaded", function() {
+    const instagramBtn = document.getElementById("instagram-button");
+    const instagramInput = document.getElementById("instagram-input");
+    const instagramIcon = document.getElementById("instagram-icon");
+    const instagramAtt = document.getElementById("instagram-link-att");
+    const savedInstagram = localStorage.getItem("saved-instagram");
+    if (savedInstagram) {
+        instagramAtt.href = savedInstagram;
+        instagramIcon.style = savedInstagram;
+        instagramInput.style.display = "none";
+        instagramBtn.textContent = "Edit";
+    }
+    instagramBtn.addEventListener("click", function() {
+        if (instagramBtn.textContent === "Submit") {
+            if (instagramInput.value.trim() !== "") {
+                let enteredURL = instagramInput.value.trim();
+                if (!enteredURL.startsWith("http://") && !enteredURL.startsWith("https://")) {
+                    enteredURL = "https://" + enteredURL;
+                }
+                instagramAtt.href = enteredURL;
+                instagramIcon.style.pointerEvents = "auto";
+                localStorage.setItem("saved-instagram", enteredURL);
+                instagramInput.style.display = "none";
+                instagramBtn.textContent = "Edit";
+            }
+        } else if (instagramBtn.textContent === "Edit") {
+            instagramAtt.removeAttribute("href");
+            instagramIcon.style.pointerEvents = "none";
+            instagramInput.style.display = "";
+            instagramBtn.textContent = "Submit";
+        }
+    })
+})
+
+function initApp() {
+    const movieCollection = JSON.parse(localStorage.getItem("movieCollection"))
+    if (!movieCollection){
+        return
+    }
+    movieCollection.forEach(movie => {
+       
+        let titleElement = $(`<p>${movie.name}</p>`);
+        let imageElement = $(`<img src="${movie.imageUrl}" alt="Image">`);
+
+        $('#movie-title').append(titleElement);
+        $('#movie-img').append(imageElement);
+    })
+}
+initApp()
